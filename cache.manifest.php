d00a9e863189a4ebd6d13d16fcd97bb761b3764c
Chris McCormick <chris@mccormick.cx>
2017-05-17 10:34:33 +0800

    Updated cache manifest with font.

--- cache.manifest.php
+++ cache.manifest.php
@@ -1,16 +1,20 @@
 <?php header('Content-Type: text/cache-manifest'); ?>
 CACHE MANIFEST
-# v20171
+# v201705171
 
 CACHE:
 index.html
 images/icon-doc.png
+images/icon.png
 js/jquery.min.js
 js/wysiwym/wysiwym.js
 js/wysiwym/wysiwym.css
 js/wysiwym/buttons.light.png
 js/coreyti-showdown/showdown.js
 js/pretty.js
+font/stylesheet.css
+font/cutivemono-regular-webfont.woff2
+font/cutivemono-regular-webfont.woff
 
 NETWORK:
 sync.php
