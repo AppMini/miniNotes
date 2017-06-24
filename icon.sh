4d8889e4649296d4c50d5271b1daf0839744f69f
Chris McCormick <chris@mccormick.cx>
2017-06-24 15:40:55 +0800

    Updated icon for clarity.

--- icon.sh
+++ icon.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-font-awesome-svg-png --nopadding --sizes 192 --color "#0C9" --icons pencil-square-o --png --dest .
-mv \#0C9/png/192/pencil-square-o.png images/icon.png
-rm -rf \#0C9
+color="#1BE3C6"
+
+font-awesome-svg-png --nopadding --sizes 192 --color "$color" --icons pencil-square-o --png --dest .
+mv "$color/png/192/pencil-square-o.png" images/icon.png
+rm -rf "$color"
